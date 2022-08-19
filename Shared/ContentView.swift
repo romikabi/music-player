2d156f804fbe1e042f6ec320bfd6264191d60c7f
Roman Abuzyarov <romikabi@yandex-team.ru>
2022-08-19 12:33:32 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,16 @@
+// Copyright 2022 Yandex LLC. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/music_playerApp.swift
@@ -0,0 +1,12 @@
+// Copyright 2022 Yandex LLC. All rights reserved.
+
+import SwiftUI
+
+@main
+struct music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
